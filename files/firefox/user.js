0947ab2ee29ae6850e5ca16366964dd2a53b130b
Erik Skultety <skultety.erik@gmail.com>
2023-08-08 17:32:34 +0200

    tasks: firefox: Add more user browser settings
    These are to limit disk caching and operations destroying the SSD.
    
    Signed-off-by: Erik Skultety <skultety.erik@gmail.com>

--- files/firefox/user.js
+++ files/firefox/user.js
@@ -1,3 +1,7 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.loadDivertedInBackground", true);
-
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("browser.sessionstore.interval", 2000000);
+user_pref("browser.cache.disk.capacity", 512000);
+user_pref("browser.cache.memory.capacity", 512000);
+user_pref("general.smoothScroll", false);
